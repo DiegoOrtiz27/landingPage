4e60e71bc7d9cb3aed481ff87bea35b20a1664e9
Diego Ortiz <emanuelortizlopez@gmail.com>
2022-08-15 18:34:25 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -42,3 +42,37 @@ async function fetchData(urlApi){
         console.log(error);
     }
 })();
+
+//Carrusel
+const grande = document.querySelector('.grande');
+const puntos = document.querySelectorAll('.punto');
+
+//1. Asignar un click a todos los puntos
+    //Saber la posicion de ese punto
+    //Aplicar un transform translateX al grande
+    //Quitar la clase activo de todos los puntos
+    //Añadir la clase activo al punto que hemos hecho click
+
+//Recorremos todos los puntos
+puntos.forEach((cadaPunto, i) => {
+    //Asignar un click a cada punto
+    puntos[i].addEventListener('click', () =>{
+        //Guardar la posicion de ese punto
+        let posicion = i;
+
+        //Calcular el porcentaje del transform
+            //Cuando la posicion es 0 transformX es 0
+            //Cuando la posicion es 1 transformX es -50%
+        let operacion = posicion * -(100/puntos.length);
+        
+        //Mover el contenedor de las imagenes
+        grande.style.transform = `translateX(${operacion}%)`;
+
+        //Recorremos los puntos para quitar la calse activo
+        puntos.forEach((cadaPunto, i) => {
+            puntos[i].classList.remove('activo');
+        });
+        //Se asigna la clase activo al buton que se dio click
+        puntos[i].classList.add('activo')
+    });
+});
